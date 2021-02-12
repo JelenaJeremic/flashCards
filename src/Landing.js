bf04badb9e055cca1e72500ac4d4b436495c17a7
JelenaJeremic <jellena.jeremic98@gmail.com>
2021-02-12 15:02:33 +0100

    commit

---
+++ src/Landing.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+
+class Landing extends Component {
+
+  render() {
+
+    return (
+        <div>
+      <div id="landing-header">
+         <h1>Welcome to Flash Cards App</h1>
+         	<button class="dugme"><a class="dugme" href="/cards">View Cards</a></button>
+        </div>
+        </div>
+
+    )
+  }
+}
+
+export default Landing;
